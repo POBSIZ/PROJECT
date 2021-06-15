c5d13e9d5251cffb5b3c2943cdcaf3d045283ea0
POBSIZ <65818008+POBSIZ@users.noreply.github.com>
2021-06-16 05:04:39 +0900

    modified main.html

--- static/js/ver.js
+++ static/js/ver.js
@@ -14,8 +14,10 @@ const animateData = {
     renderer: "svg",
     loop: false,
     autoplay: true,
-    rendererSettings: { progressiveLoad: false },
-    path: jsonPath, 
+    rendererSettings: {
+        progressiveLoad: false
+    },
+    path: jsonPath,
 };
 
 let anim = lottie.loadAnimation(animateData);
@@ -23,30 +25,59 @@ let anim = lottie.loadAnimation(animateData);
 const navBar = document.querySelector('.navBar');
 scene.on("update", (e) => {
     // console.log(e.scrollPos)
-    if(e.scrollPos > 0){
-        navBar.classList.add('sticky');
-    }else if(e.scrollPos == 0){
-        navBar.classList.remove('sticky');
-    }
 
     // INFO
-    if(e.scrollPos > 410){
+    if (e.scrollPos > 150) {
         document.querySelector('.info_tab').setAttribute('style', 'top: 0; opacity: 100%;');
     }
-
-    // DO-LIST
-    doList = document.querySelectorAll('.doList-itm')
-    doListM = 400
-    if(e.scrollPos > doList[0].offsetTop - doList[0].offsetHeight - doListM){
-        doList[0].classList.add('scrollActive');
-    }
-    if(e.scrollPos > doList[1].offsetTop - doList[1].offsetHeight - doListM){
-        doList[1].classList.add('scrollActive');
-    }
-    if(e.scrollPos > doList[2].offsetTop - doList[2].offsetHeight - doListM){
-        doList[2].classList.add('scrollActive');
-    }
-    if(e.scrollPos > doList[3].offsetTop - doList[3].offsetHeight - doListM){
-        doList[3].classList.add('scrollActive');
-    }
 });
+
+var sts = 3;
+window.onload = () => {
+    if ($(window).width()+17 > 1000){
+        sts = 3;
+        console.log(3);
+    }else{
+        sts = 1;
+        console.log(1);
+    }
+
+    $('.doList').slick({
+        infinite: true,
+        slidesToShow: sts,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+    
+    if(sts == 3){
+        $('.doList').on('afterChange', function (event, slick, direction) {
+            const active = document.querySelectorAll('.slick-active');
+            const centPick = document.querySelectorAll('.centOn');
+            
+            const cent0 = active[0];
+            const cent1 = active[1];
+            
+            // console.log(active.length);
+            
+            if (centPick.length == 2) {
+                centPick[1].classList.remove('centOn');
+            }
+            
+            cent0.classList.remove('centOn');
+            cent1.classList.add('centOn');
+            
+        });
+    }else{
+        const dli = document.querySelectorAll('.doList-itm');
+        dli.forEach(i =>{
+            i.classList.add('centOn')
+        });
+    }
+
+    var cent1s = document.querySelectorAll('.slick-active')[1];
+    cent1s.classList.add('centOn');
+}
+
+
