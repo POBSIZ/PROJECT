c5686ef3a63d7f8f2123f2ca880d73b98a11a2f8
POBSIZ <65818008+POBSIZ@users.noreply.github.com>
2021-06-30 00:23:39 +0900

    fix-navTab error

--- static/js/base/nav.js
+++ static/js/base/nav.js
@@ -1,5 +1,6 @@
 const moblieNav = document.querySelector('.mobile-navList');
 const navTab = document.querySelector('.navTab');
 navTab.addEventListener('click', ()=>{
+    console.log('dasdas')
     moblieNav.classList.toggle('on');
 });
