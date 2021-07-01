e7586da2f78f5077bc965be4ec0118cd774bd1ae
POBSIZ <65818008+POBSIZ@users.noreply.github.com>
2021-07-02 04:17:18 +0900

    add some table at calendarapp models.py

--- calendarapp/models.py
+++ calendarapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models.fields import DateField, IntegerField
+from django.db.models.fields.related import ForeignKey
 
-# Create your models here.
+class YMD(models.Model):
+    full_date = DateField
+
+class Year(models.Model):
+    year = IntegerField()
+
+class Month(models.Model):
+    month = IntegerField()
+    o_year = models.ForeignKey(Year)
+
+class Day(models.Model):
+    day = IntegerField()
+    o_month = models.ForeignKey(Month)
+    o_ymd = models.ForeignKey(YMD, blank=True, null=True)
+
